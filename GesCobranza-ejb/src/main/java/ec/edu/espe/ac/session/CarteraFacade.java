f795e3da9bde634246dcd7ae61c0079b9c8d897f
Fernando Acurio <fmacurio@espe.edu.ec>
2018-11-11 22:07:19 -0500

    Segmentación del nuevo modelo de BD según criterios de Producto, Días de Mora y Monto. (Completo)

--- GesCobranza-ejb/src/main/java/ec/edu/espe/ac/session/CarteraFacade.java
+++ GesCobranza-ejb/src/main/java/ec/edu/espe/ac/session/CarteraFacade.java
@@ -2,8 +2,12 @@ package ec.edu.espe.ac.session;
 
 import ec.edu.espe.ac.model.Agente;
 import ec.edu.espe.ac.model.Cartera;
+import ec.edu.espe.ac.model.Permora;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -21,6 +25,12 @@ public class CarteraFacade extends AbstractFacade<Cartera> implements CarteraFac
 
     @PersistenceContext(unitName = "ec.edu.espe.ac_GesCobranza-ejb_ejb_1PU")
     private EntityManager em;
+
+    @EJB
+    private AgenteFacadeLocal agenteFacade;
+
+    @EJB
+    private PermoraFacadeLocal permoraFacade;
 
     @Override
     protected EntityManager getEntityManager() {
@@ -47,10 +57,9 @@ public class CarteraFacade extends AbstractFacade<Cartera> implements CarteraFac
         factory.close();
         return list;
     }
-    
-    
+
     @Override
-        public List<Cartera> CarteraFiltro(Agente codigo) {
+    public List<Cartera> CarteraFiltro(Agente codigo) {
         /*EntityManagerFactory factory = Persistence.createEntityManagerFactory("ec.edu.espe.ac_GesCobranza-ejb_ejb_1PU");
         EntityManager em1 = factory.createEntityManager();
         //pkg_jpa.Autor c1=new pkg_jpa.Autor();
@@ -66,11 +75,194 @@ public class CarteraFacade extends AbstractFacade<Cartera> implements CarteraFac
         factory.close();
         //List<Cartera> list;
         //list = em.createNamedQuery("Cartera.findByName").setParameter("name", codigo).getResultList();
-*/        
+         */
         Query qry = this.em.createQuery("SELECT obj FROM Cartera obj WHERE obj.codigoagente=?1");
         qry.setParameter(1, codigo);
         return qry.getResultList();
         //return list;
     }
 
+    
+    @Override
+    public int insertarConfigDia() {
+        //EntityManagerFactory factory = Persistence.createEntityManagerFactory("ec.edu.espe.ac_GesCobranza-ejb_ejb_1PU");
+        //EntityManager em1 = factory.createEntityManager();
+        List<Agente> agentesTodos = agenteFacade.listaAgenteTodos();
+        List<Permora> carteraTodos = permoraFacade.listaPermoraTodos();
+        Cartera c1 = new Cartera();
+
+        List<Agente> usersNov = new ArrayList<Agente>();
+        List<Agente> usersMed = new ArrayList<Agente>();
+        List<Agente> usersExp = new ArrayList<Agente>();
+        int res = 0;
+
+        try {
+            //em1.getTransaction().begin();
+            if (agentesTodos != null) {
+                for (Agente opcion : agentesTodos) {
+                    if (opcion.getTipo().equals("novato")) {
+                        usersNov.add(opcion);
+                    }
+                    if (opcion.getTipo().equals("medio")) {
+                        usersMed.add(opcion);
+                    }
+                    if (opcion.getTipo().equals("experto")) {
+                        usersExp.add(opcion);
+                    }
+                }
+            }
+            List<Permora> carNov = new ArrayList<Permora>();
+            List<Permora> carMed = new ArrayList<Permora>();
+            List<Permora> carExp = new ArrayList<Permora>();
+
+            if (carteraTodos != null) {
+                for (Permora opcion : carteraTodos) {
+                    if (opcion.getNombreproducto().equals("CC") || opcion.getNombreproducto().equals("vehicular")) {
+                        if (opcion.getMonto().intValue() < 20000 && opcion.getDiasmora().intValue() < 90) {
+                            carNov.add(opcion);
+                        }
+                        if (opcion.getMonto().intValue() < 20000 && opcion.getDiasmora().intValue() >= 90) {
+                            carMed.add(opcion);
+                        }
+                        if (opcion.getMonto().intValue() >= 20000 && opcion.getDiasmora().intValue() < 90) {
+                            carMed.add(opcion);
+                        }
+                        if (opcion.getMonto().intValue() >= 20000 && opcion.getDiasmora().intValue() >= 90) {
+                            carExp.add(opcion);
+                        }
+                    }
+                    if (opcion.getNombreproducto().equals("hipotecario") || opcion.getNombreproducto().equals("comercial")) {
+                        if (opcion.getMonto().intValue() < 30000 && opcion.getDiasmora().intValue() < 90) {
+                            carNov.add(opcion);
+                        }
+                        if (opcion.getMonto().intValue() < 30000 && opcion.getDiasmora().intValue() >= 90) {
+                            carMed.add(opcion);
+                        }
+                        if (opcion.getMonto().intValue() >= 30000 && opcion.getDiasmora().intValue() < 90) {
+                            carMed.add(opcion);
+                        }
+                        if (opcion.getMonto().intValue() >= 30000 && opcion.getDiasmora().intValue() >= 90) {
+                            carExp.add(opcion);
+                        }
+                    }
+                }
+            }
+            if (carteraTodos != null && agentesTodos != null) {
+                distribucion(usersNov, carNov);
+                distribucion(usersMed, carMed);
+                distribucion(usersExp, carExp);
+                /*
+                
+                for (Agente opcionUser : agentesTodos) {
+                    
+                    if (opcionUser.getTipo().equals("novato")) {
+                        for (Permora opcion : carNov) {
+                            if (numeroConfigPorUser(opcionUser) <= ((int) (carteraTodos.size() / usersNov))) {
+                                if (numeroConfigPorCartera(opcion) == 0) {
+                                    c1.setId(opcionUser);
+                                    c1.setCodigocartera(opcion);
+                                    c1.setAccion("SMS");
+                                    c1.setCostollamada(BigInteger.valueOf(1l));
+                                    c1.setEstado("Pendiente");
+                                    em.persist(c1);
+                                }
+                            }
+                        }
+                    }
+                
+                }*/
+            }
+            //em1.getTransaction().commit();
+            //ssss
+
+            res = 1;
+        } catch (Exception ex) {
+            res = 0;
+
+        }
+        //em1.close();
+        //factory.close();
+        return res;
+    }
+
+    public int distribucion(List<Agente> listAgente, List<Permora> listPermora) {
+        List<Permora> originalList = listPermora;
+        int car = listPermora.size();
+        int usu = listAgente.size();
+        int partitionSize = 0;
+
+        double aux = 0;
+        int res = 0;
+
+        if (usu != 0) {
+            aux = (double) car / usu;
+            res = (car % usu);
+        }
+
+        System.out.println(aux);
+        if ((aux - Math.floor(aux)) < 0.5) {
+            partitionSize = (int) Math.floor(aux);
+        }
+        if ((aux - Math.floor(aux)) >= 0.5) {
+            partitionSize = (int) Math.ceil(aux);
+        }
+
+        List<List<String>> partitions = new ArrayList<List<String>>();
+        int fin = 0;
+        List<Permora> auxList = new ArrayList<Permora>();
+        int agenteAct = 0;
+
+        if ((aux - Math.floor(aux)) >= 0.5) {
+            for (int i = 0; i < originalList.size(); i += partitionSize) {
+                auxList = originalList.subList(i,
+                        Math.min(i + partitionSize, originalList.size()));
+                fin = Math.min(i + partitionSize, originalList.size());
+
+                for (Permora perAct : auxList) {
+                    insertaCartera(listAgente.get(agenteAct), perAct);
+                }
+                agenteAct++;
+            }
+        }
+        if ((aux - Math.floor(aux)) < 0.5) {
+            for (int i = 0; i < originalList.size(); i += partitionSize) {
+                if (res > 0) {
+                    auxList = originalList.subList(i,
+                            (Math.min(i + partitionSize, originalList.size())) + 1);
+                    System.out.println(res);
+                    res--;
+                    i++;
+                    for (Permora perAct : auxList) {
+                        insertaCartera(listAgente.get(agenteAct), perAct);
+                    }
+                    agenteAct++;
+                } else {
+                    auxList = originalList.subList(i,
+                            Math.min(i + partitionSize, originalList.size()));
+                    fin = Math.min(i + partitionSize, originalList.size());
+                    for (Permora perAct : auxList) {
+                        insertaCartera(listAgente.get(agenteAct), perAct);
+                    }
+                    agenteAct++;
+                }
+            }
+        }
+
+        System.out.println(partitions.toString());
+        return 0;
+    }
+
+    public int insertaCartera(Agente agente, Permora permora) {
+        Cartera c1 = new Cartera();
+        try {
+            c1.setCodigoagente(agente);
+            c1.setCodigopermora(permora);
+            c1.setEstadoasig(BigInteger.ZERO);
+            c1.setCodigocar(BigDecimal.ZERO);
+            em.persist(c1);
+            return 1;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 }
--- GesCobranza-ejb/src/main/java/ec/edu/espe/ac/session/CarteraFacadeLocal.java
+++ GesCobranza-ejb/src/main/java/ec/edu/espe/ac/session/CarteraFacadeLocal.java
@@ -34,5 +34,5 @@ public interface CarteraFacadeLocal {
     
     List<Cartera> listaCarteraTodos();
     List<Cartera> CarteraFiltro(Agente codigo);
-    
+    public int insertarConfigDia();
 }
